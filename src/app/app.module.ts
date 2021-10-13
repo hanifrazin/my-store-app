d42149762f5408143170c9895496df88393ca184
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2021-10-13 22:10:42 +0700

    Add navigation view product details is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,10 +20,10 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{
-      path: '',
-      component: ProductListComponent
-    }]),
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
